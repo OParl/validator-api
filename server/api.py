5f261d05e793cbd52d10396b182451e1acfe8a76
Stefan Graupner <efrane@meanderingsoul.com>
2017-12-22 10:55:37 +0100

    ++Code structure

---
+++ server/api.py
@@ -0,0 +1,43 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2017 Stefan Graupner
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from flask import request, url_for
+from flask_api import FlaskAPI, status, exceptions
+from oparl_validator.core.validator import Validator
+
+global app
+app = FlaskAPI(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return []
+
+@app.route('/validate', methods=['POST'])
+def validate():
+    return []
+
+@app.route('/result/<string:uuid>', methods=['GET'])
+def result(uuid):
+
+    return []
